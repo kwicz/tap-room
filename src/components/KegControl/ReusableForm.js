4c3882d784e9ff1af93d75c6a0813117d018b73b
Katy Solovewicz <katy@solovewicz.com>
2020-04-24 11:29:36 -0700

    add Reusable Form

---
+++ src/components/KegControl/ReusableForm.js
@@ -0,0 +1,35 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function ReusableForm(props) {
+  return (
+    <React.Fragment>
+      <form onSubmit= {props.formSubmissionHandler}>
+        <input 
+          type='text'
+          name='name'
+          placeholder='Beer Name'/>
+        <input 
+          type='text'
+          name='brand'
+          placeholder='Brand'/>
+        <input 
+          type='number'
+          name='alcoholContent'
+          placeholder='Alcohol Content'/>
+        <input 
+          type='number'
+          name='price'
+          placeholder='Price'/>
+        <button type="submit">{props.buttonText}</button>
+      </form>
+    </React.Fragment>
+  )
+}
+
+ReusableForm.propTypes = {
+  formSubmissionHandler: PropTypes.func,
+  buttonText: PropTypes.string
+};
+
+export default ReusableForm;
