43f73e7060663e0b97558a4c435756c8fcc2ceca
Katy Solovewicz <katy@solovewicz.com>
2020-04-25 22:25:10 -0700

    Updates

---
+++ precache-manifest.2506a4fbec27bd000caf48ad7f59636d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b26b50ea4dadd245e6144167fa9b4db",
+    "url": "/tap-room/index.html"
+  },
+  {
+    "revision": "5a68802479f1b93ee87c",
+    "url": "/tap-room/static/css/2.f426b0f3.chunk.css"
+  },
+  {
+    "revision": "f92588bcbe73e1394ea8",
+    "url": "/tap-room/static/css/main.121dd35d.chunk.css"
+  },
+  {
+    "revision": "5a68802479f1b93ee87c",
+    "url": "/tap-room/static/js/2.702dacd3.chunk.js"
+  },
+  {
+    "revision": "f92588bcbe73e1394ea8",
+    "url": "/tap-room/static/js/main.6e053a80.chunk.js"
+  },
+  {
+    "revision": "9ae27e9e5dcbec52697c",
+    "url": "/tap-room/static/js/runtime-main.4cf27436.js"
+  }
+]);
