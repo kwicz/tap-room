e77f8a97c15e6931c1d918b8d26d4c3fe0138434
Katy Solovewicz <katy@solovewicz.com>
2020-04-25 19:53:16 -0700

    add functionality to update form

--- src/components/KegControl/EditKegForm.js
+++ src/components/KegControl/EditKegForm.js
@@ -3,9 +3,16 @@ import ReusableForm from "./ReusableForm";
 import PropTypes from 'prop-types';
 
 function EditKegForm (props) {
+
+  function handleEditKegFormSubmission(event) {
+    event.preventDefault();
+    props.onEditKeg({name: event.target.name.value, brand: event.target.brand.value, alcoholContent: event.target.alcoholContent.value, price: event.target.price.value
+    });
+  }
   return (
     <React.Fragment>
       <ReusableForm 
+        formSubmissionHandler={handleEditKegFormSubmission}
         buttonText="Update Keg" />
     </React.Fragment>
   );
